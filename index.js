e2bd985cbd4960212dc9694b12c03b6f2711ab9c
LavrL <llembersk@gmail.com>
2020-02-25 11:12:50 +0200

    updated index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
