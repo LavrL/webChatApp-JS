21c9917bfab089a103ace85ca93fa4c61a522f0e
LavrL <llembersk@gmail.com>
2020-03-16 16:06:37 +0200

    removed jquery

--- public/js/socketScript.js
+++ public/js/socketScript.js
@@ -1,61 +1,52 @@
 const socket = io();
-var name = parseSubmitRequest("username") || 'Anonymous';
-var room = parseSubmitRequest("room") || 'No Room Selected';
+const name = parseSubmitRequest("username") || 'Anonymous';
+const room = parseSubmitRequest("room") || 'No Room Selected';
 
 const roomTitle = document.querySelector('.room-title');
 roomTitle.innerHTML = 'channel - ' + room;
-//$(".room-title").text('channel - ' + room);
-//$(".avatar-name").text(name);
+
 const avatarName = document.querySelector('.avatar-name');
 avatarName.innerHTML = name;
 
-// $(document).ready(function () {
-//     var socket = io();
-//     $('form').submit(function () {
-//         socket.emit('chat message', {
-//             msg: $('#m').val(),
-//             room: room,
-//             name: name
-//         });
-//         $('#m').val('');
-//         return false;
-//     })
-// });
-const formChat = document.getElementById('btn');
-console.log(formChat);
-formChat.addEventListener('click', funcSubmit);
+const userMessageStyle = [
+    'font-size: 16px',
+    'color: black',
+    'padding-left: 10px'].join(';');
 
-function funcSubmit() {
-    //event.preventDefault();
+const funcSubmit = () => {
     socket.emit('chat message', {
-        msg: $('#m').val(),
+        msg: document.getElementById('postedMessage').value,
         room: room,
         name: name
     });
-    $('#m').val('');
+    document.getElementById('postedMessage').value = '';
     location.reload();
     return false;
 };
 
+const formChat = document.getElementById('btn');
+formChat.addEventListener('click', funcSubmit);
 
-$(document).ready(function () {
+const fetchChatMessages = () => {
     fetch("/chats")
         .then(handleErrors)
         .then(json => {
-            // console.log(json)
             json.map(data => {
-                let userMessageStyle = [
-                    'font-size: 16px',
-                    'color: black',
-                    'padding-left: 10px'
-                ].join(';');
-                var userMessage = $('<span />').attr('style', userMessageStyle)
-                    .html(data.message);
-                var userSaid = $('<span />').html(data.sender + ' says: ');
-                $('#messages').append($('<li>')
-                    .css("color", "blue")
-                    .append(userSaid)
-                    .append(userMessage));
+                const userMessage = document.createElement('span');
+                userMessage.setAttribute('style', userMessageStyle);
+                userMessage.innerHTML = data.message;
+
+                const userSaid = document.createElement('span');
+                userSaid.innerHTML = data.sender + ' says: ';
+
+                const messages = document.getElementById('messages');
+                const messagesLi = document.createElement('li');
+
+                messagesLi.setAttribute('style', 'color: blue');
+                messagesLi.appendChild(userSaid);
+                messagesLi.appendChild(userMessage);
+                messages.appendChild(messagesLi);
+
             });
             let objDiv = document.getElementById("messages");
             objDiv.scrollTop = objDiv.scrollHeight;
@@ -63,24 +54,27 @@ $(document).ready(function () {
         .catch(error => {
             console.log("Error = " + error)
         });
-});
+};
 
 socket.on('chat message', function (msg) {
     console.log('typed message', msg.message);
-    let userMessageStyle = [
-        'font-size: 16px',
-        'color: black',
-        'padding-left: 10px'
-    ].join(';');
 
-    var userMessage = $('<span />').attr('style', userMessageStyle)
-        .html(msg.message);
-    var userSaid = $('<span />').html(msg.sender + ' says: ');
-    $('#messages').append($('<li>')
-        .css("color", "blue")
-        .append(userSaid)
-        .append(userMessage));
-    $('#messages').animate({ scrollTop: $('#messages').prop("scrollHeight") }, 500);
+    const userMessage = document.createElement('span');
+    userMessage.setAttribute('style', userMessageStyle);
+    userMessage.innerHTML = data.message;
+
+    const userSaid = document.createElement('span');
+    userSaid.innerHTML = data.sender + ' says: ';
+
+
+    const messages = document.getElementById('messages');
+    const messagesLi = document.createElement('li');
+    messagesLi.setAttribute('style', 'color: blue');
+    messagesLi.appendChild(userSaid);
+    messagesLi.appendChild(userMessage);
+    messages.appendChild(messagesLi);
+
+    messages.animate({ scrollTop: messages.prop("scrollHeight") }, 500);
 });
 
 // fires when client successfully connects to the server
@@ -93,10 +87,8 @@ socket.on("connect", function () {
     });
 });
 
-// socket.on("message", function (msg) {
-//     console.log("new message ");
-//     var $messages = $("#messages");
-//     var $message = $('<li></li>');
-//     $message.append("<p>" + msg.text + "</p>");
-//     $messages.append($message);
-// })
+const main = () => {
+    fetchChatMessages();
+};
+
+main();
