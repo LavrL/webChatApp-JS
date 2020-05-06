758ab99e2f3a8d1431cd8c46a1c266d360812bc9
LavrL <llembersk@gmail.com>
2020-05-06 21:32:43 +0300

    fixed test

--- public/__test__/frontChat.test.js
+++ public/__test__/frontChat.test.js
@@ -1,38 +1,25 @@
 const { parseSubmitRequest, handleErrors, funcSubmit } = require('../js/script');
 const fetchMock = require('fetch-mock');
 
-test('checks if parseSubmitRequest function exists', () => {
-    expect(parseSubmitRequest).toBeDefined();
-});
+describe('FrontChat testing', () => {
+    test('checks if parseSubmitRequest function exists', () => {
+        expect(parseSubmitRequest).toBeDefined();
+    });
 
-test('checks if parseSubmitRequest works', () => {
-    let variable = 'username';
-    expect(parseSubmitRequest(variable)).toBe('lavr1');
+    test('checks if parseSubmitRequest works', () => {
+        let variable = 'username';
+        expect(parseSubmitRequest(variable)).toBe('lavr1');
 
-    variable = 'user';
-    expect(parseSubmitRequest(variable)).toBe(undefined);
-});
+        variable = 'user';
+        expect(parseSubmitRequest(variable)).toBe(undefined);
+    });
 
-test('check handleError function', () => {
-    var successMessage = { ok: false, status: '1' };
+    test('check handleError function', () => {
+        var successMessage = { ok: false, status: '1', json: jest.fn() };
+        expect(() => handleErrors(successMessage)).toThrow('1');
 
-    successMessage.ok = true;
-    expect(() => handleErrors(successMessage)).toThrow();
+        successMessage.ok = true;
+        expect(handleErrors(successMessage)).toBe(successMessage.json());
+    });
 
-    successMessage.ok = false;
-    expect(() => handleErrors(successMessage)).toThrow();
-});
-// test('submit function with no parameters', () => {
-//     //spyOn(funcSubmit, 'funcSubmit');
-//     // const mockFn = jest.fn().mockImplementation(funcSubmit);
-//     // // c.circumference(2);
-//     // expect(mockFn.mock.calls[0][0]).toEqual(1);
-
-// })
-
-// test('checks if handleErrors works in fetch request', async () => {
-//     fetchMock.get("http://localhost:3000/chats", {hello: "world"});
-//     const response = await fetch('http://localhost:3000/chats');
-//     console.log('response ', await response.json());
-//     expect(handleErrors).toContain(response);
-// })
+})
